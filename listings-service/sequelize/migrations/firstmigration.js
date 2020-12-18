63ea7f2114138233de5637121ab22c12c4bb65c4
js <juliano.silva@nkey.com.br>
2020-12-18 13:37:30 -0300

    added migration, and sequelize

---
+++ listings-service/sequelize/migrations/firstmigration.js
@@ -0,0 +1,63 @@
+module.exports.up = (queryInterface, DataTypes) => {
+
+    return queryInterface.createTable("listings", {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER.UNSIGNED
+        },
+
+        first_name: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        last_name: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        email: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        gender: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        company: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        city: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        title: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        lat: {
+            allowNull: true,
+            type: DataTypes.STRING
+        },
+        long: {
+            allowNull: true,
+            type: DataTypes.STRING
+        },
+    },
+    {
+        charset: "utf8"
+    })
+
+}
+
+// "id": 1,
+// "first_name": "Laura",
+// "last_name": "Richards",
+// "email": "lrichards0@reverbnation.com",
+// "gender": "Female",
+// "company": "Meezzy",
+// "city": "Warner, NH",
+// "title": "Biostatistician III",
+// "lat": "43.3044615",
+// "long": "-71.9650652" 
